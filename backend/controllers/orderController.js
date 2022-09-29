acafc3914aba351734c47b2f9970d842575ea68c
DESKTOP-ERETTBS\Chathuka Heshan <chathukaheshan@gmail.com>
2022-09-29 22:51:42 +0530

    created .env file

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,9 +1,7 @@
 import asyncHandler from 'express-async-handler';
 import Order from '../models/orderModel.js';
 
-// @desc    Create new order
-// @route   POST /api/orders
-// @access  Private
+
 const addOrderItems = asyncHandler(async (req, res) => {
   const {
     orderItems,
