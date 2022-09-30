545569531bd5ee8dc275ccbf3c1f0fdb65a7ff09
Dinithi Hiththatiyage <88882507+IT20162696@users.noreply.github.com>
2022-09-30 11:18:59 +0530

    Update productController.js
    Added delete product by id

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -33,3 +33,15 @@ const getProductById = asyncHandler(async (req, res) => {
     throw new Error('Product not found');
   }
 });
+
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    await product.remove();
+    res.json({ message: 'Product removed' });
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+});
