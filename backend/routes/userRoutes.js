901bf4c4622eda2a399ecf6796b06923266ae1bb
DESKTOP-ERETTBS\Chathuka Heshan <chathukaheshan@gmail.com>
2022-10-06 22:04:15 +0530

    authorization middleware created and updated user route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,19 @@
 import express from 'express';
 const router = express.Router();
-
+import {
+    
+    getUserProfile,
+    registerUser,
+    updateUserProfile,
+    getUsers,
+    
+  } from '../controllers/userController.js';
+  import { admin, protect } from '../middleware/authMiddleware.js';
 
 router.route('/').post(registerUser).get(protect, admin, getUsers);
-
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default router;
