563a3fccbf2c0e9437e38eeceec33f0f49b91151
Dinithi Hiththatiyage <88882507+IT20162696@users.noreply.github.com>
2022-10-11 22:40:39 +0530

    Update productActions.js
    Updateproduct

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -96,3 +96,35 @@ export const listProducts = (keyword = '', pageNumber = '') => async (dispatch) 
       });
     }
   };
+
+  export const updateProduct = (product) => async (dispatch, getState) => {
+    try {
+      dispatch({ type: PRODUCT_UPDATE_REQUEST });
+  
+      const {
+        userLogin: { userInfo },
+      } = getState();
+  
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      };
+  
+      const { data } = await axios.put(`/api/products/${product._id}`, product, config);
+  
+      dispatch({
+        type: PRODUCT_UPDATE_SUCCESS,
+        payload: data,
+      });
+      dispatch({ type: ADD_SNACKBAR, payload: 'Product updated successfully!' });
+    } catch (error) {
+      dispatch({
+        type: PRODUCT_UPDATE_FAIL,
+        payload:
+          error.response && error.response.data.message ? error.response.data.message : error.message,
+      });
+    }
+  };
+  
