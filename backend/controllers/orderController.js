404b4cd5dcb17cfb6c8bee862b8793919e6643f7
OmeshaDharmarathna <it20007188@my.sliit.lk>
2022-10-14 09:16:43 +0700

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -108,8 +108,7 @@ const updateOrderToDelivered = asyncHandler(async (req, res) => {
 
     res.json(updatedOrder);
   } else {
-    res.status(404);
-    throw new Error('Order not found');
+    
   }
 });
 
